52059ba8d54c818d9e23d92a1290a46c49833928
shoebillk <blue021@gmail.com>
2019-07-15 17:59:33 +0900

    daily leet

--- 64.minimum-path-sum.go
+++ 64.minimum-path-sum.go
@@ -32,7 +32,75 @@
  * 
  * 
  */
+
+func pathSum(min *int, grid [][]int, i, j int, temp int, cache [][]int) int { 
+	m:= len(grid)
+	n:= len(grid[0])
+
+	if i == m-1 && j == n-1 {
+		if *min == 0 {
+			*min = temp + grid[i][j]
+		}
+		if t := temp + grid[i][j]; *min > t {
+			*min = t
+		}
+		return grid[i][j]
+	}
+
+	if i >=m || j >= n {
+		return -1
+	}
+
+	if c := cache[i][j]; c > -1 {
+		if *min == 0 {
+			*min = temp + c 
+		}
+		if t := temp + c; *min > t {
+			*min = t
+		}
+		return  c
+	}
+
+	a := pathSum(min, grid, i+1, j, temp+grid[i][j], cache)
+	b := pathSum(min, grid, i, j+1, temp+grid[i][j], cache)
+
+	r := 0
+	if a == -1 {
+		r = b
+	}  else {
+		if b == -1 {
+			r = a
+		} else {
+			if a > b {
+				r = b
+			} else {
+				r = a
+			}
+		}
+	}
+
+
+	cache[i][j] = r + grid[i][j]
+	return r + grid[i][j]
+}
+
 func minPathSum(grid [][]int) int {
+	ret := 0
+
+	m:= len(grid)
+	n:= len(grid[0])
+
+	cache := make([][]int, m)
+	for i,_:= range(cache) {
+		cache[i] = make([]int, n)
+		for j, _:= range(cache[i]) {
+			cache[i][j] = -1
+		}
+	}
+
+	pathSum(&ret, grid, 0,0,0, cache)
+
+	return ret
     
 }
 
