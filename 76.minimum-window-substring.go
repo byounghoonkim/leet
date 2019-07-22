dbc291d832b7ac7f88e90fdb32554a74d38470af
BH Kim <blue021@gmail.com>
2019-07-22 22:39:35 +0900

    daily leet

--- 76.minimum-window-substring.go
+++ 76.minimum-window-substring.go
@@ -32,8 +32,81 @@
  * one unique minimum window in S.
  * 
  * 
+
+ "ADOBECODEBANC", T = "ABC"
+  1         1
+	 1     1   
+	   1      1
+
  */
+
+func minMax(t []int) (int, int) {
+	min, max := t[0], t[0]
+
+	for _, a := range(t) {
+		if min > a {
+			min = a
+		}
+		if max < a {
+			max = a
+		}
+	}
+
+	return min, max
+}
+
+func spanLen(t []int) int {
+	min, max := minMax(t)
+	return max - min
+}
+
+func findWindow(m [][]int, t []int, min *int, r *[]int ) {
+	if 0 == len(m) {
+		if *min == -1 || *min > spanLen(t) {
+			*min = spanLen(t)
+			*r = make([]int, len(t))
+			copy(*r,t)
+		}
+		return
+	}
+
+	for _, a := range(m[0]) {
+		findWindow(m[1:], append(t, a), min, r)
+	}
+}
+
 func minWindow(s string, t string) string {
-    
+
+	m := make([][]int, len(t))
+
+
+	for i, sc := range(s) {
+		for j, tc := range(t) {
+			if sc == tc {
+				m[j] = append(m[j], i)
+			}
+		}
+	}
+
+
+	for _, a := range(m) {
+		if len(a) == 0 {
+			return ""
+		}
+	}
+
+
+	min := -1
+	ret := []int{}
+
+	findWindow(m, []int{}, &min, &ret)
+
+	min, max := minMax(ret)
+
+	if max+1 - min < len(m) {
+		return ""
+	}
+	
+	return s[min:max+1]
 }
 
