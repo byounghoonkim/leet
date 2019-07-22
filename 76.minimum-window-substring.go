2f042390ccf3c882dad187a5f9d3b54eca0d9466
shoebillk <blue021@gmail.com>
2019-07-22 14:47:35 +0900

    daily leet

---
+++ 76.minimum-window-substring.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=76 lang=golang
+ *
+ * [76] Minimum Window Substring
+ *
+ * https://leetcode.com/problems/minimum-window-substring/description/
+ *
+ * algorithms
+ * Hard (31.33%)
+ * Likes:    2467
+ * Dislikes: 165
+ * Total Accepted:    252.5K
+ * Total Submissions: 806K
+ * Testcase Example:  '"ADOBECODEBANC"\n"ABC"'
+ *
+ * Given a string S and a string T, find the minimum window in S which will
+ * contain all the characters in T in complexity O(n).
+ * 
+ * Example:
+ * 
+ * 
+ * Input: S = "ADOBECODEBANC", T = "ABC"
+ * Output: "BANC"
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * If there is no such window in S that covers all characters in T, return the
+ * empty string "".
+ * If there is such window, you are guaranteed that there will always be only
+ * one unique minimum window in S.
+ * 
+ * 
+ */
+func minWindow(s string, t string) string {
+    
+}
+
