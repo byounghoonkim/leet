c1a87202db8477e6de07b6a916ceb1f4837f1d2a
BH Kim <blue021@gmail.com>
2019-07-30 01:05:19 +0900

    daily leet

--- 82.remove-duplicates-from-sorted-list-ii.go
+++ 82.remove-duplicates-from-sorted-list-ii.go
@@ -11,42 +11,39 @@
  * }
  */
 
-func next(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
 
-	if head.Next == nil {
-		return head
-	}
-
-	if head.Val != head.Next.Val {
-		return head
-	}
-
-	cur := head
-	for  cur.Next != nil || cur.Next.Val == cur.Val {
+//  next 메소드는 파라미터로 넘어온 노드를 기준으로 다음 항목을 리턴해 준다.
+// 파라미터의 노드 가 닐이거나 다음 노드가 닐이 일때는 자기 자신을 리턴한다.
+// 노드가 다음 노드와 값이 다를 때도 자기 자신을 리턴한다.
+// 그렇지 않은 경우는 현재 노드와 다음 노드의 값이 일치 하는 경우이므로
+// 다음 노드가 닐이거나 일치 하지 않을 때까지 진행한다.
+// 그리고 얻어진 노드를 (이 노드도 중복 값이 배열되어 있을 수 있으므로)
+// 재귀 호출하여 결과를 리턴한다.
+func next(cur *ListNode) *ListNode {
+	if cur == nil { return nil }
+	if cur.Next == nil { return cur}
+	if cur.Next.Val != cur.Val { return cur }
+	for  cur.Next != nil {
+		if cur.Next.Val != cur.Val {
+			break
+		}
 		cur = cur.Next
 	}
 
-	if cur.Next == nil {
-		return nil
-	}
-
-	return next(cur.Next.Next)
+	return next(cur.Next)
 }
 func deleteDuplicates(head *ListNode) *ListNode {
-
 	ret := next(head)
+	if ret == nil { return ret }
 
 	cur := ret
-	for n := next(cur.Next) ; n != nil; {
-		fmt.Println("cur", cur)
-		fmt.Println("cur.Next", cur.Next)
-		fmt.Println("n", n)
-		fmt.Println("||||")
+	for ;;{
+		n := next(cur.Next)
 		cur.Next = n
-		cur = cur.Next
+		if n == nil {
+			break
+		}
+		cur = n
 	}
 
 	return ret
