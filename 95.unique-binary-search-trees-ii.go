68ffd566ef9a46e7947ead10015dffd838e8e963
BH Kim <blue021@gmail.com>
2019-08-16 00:58:41 +0900

    daily leet

---
+++ 95.unique-binary-search-trees-ii.go
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=95 lang=golang
+ *
+ * [95] Unique Binary Search Trees II
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (36.58%)
+ * Likes:    1388
+ * Dislikes: 119
+ * Total Accepted:    148.1K
+ * Total Submissions: 404.8K
+ * Testcase Example:  '3'
+ *
+ * Given an integer n, generate all structurally unique BST's (binary search
+ * trees) that store values 1 ... n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output:
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+
+func gen(n int, b int) []*TreeNode {
+	if n == 0 {
+		return []*TreeNode{ nil }
+	}
+	if n == 1 {
+		return []*TreeNode{ { Val: b, } }
+	}
+
+	result := []*TreeNode{}
+
+	for i := 0 ; i < n; i++ {
+
+		lefts := gen(i, b) 
+		rights := gen(n-1-i, b+i+1)
+
+		for _, l := range lefts {
+			for _, r := range rights{
+				node := TreeNode{
+					Val: b+i,
+					Left: l,
+					Right: r,
+				}
+
+				result = append(result, &node)
+			}
+		}
+	}
+
+	return result
+}
+
+func generateTrees(n int) []*TreeNode {
+
+	if n == 0 {
+		return []*TreeNode{}
+	}
+	
+	return gen(n, 1)
+}
+
